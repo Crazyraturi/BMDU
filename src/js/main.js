94ee60b941c2ec9bd9ed5377a15b9acc27be68a4
crazyraturi <subodhblogger966@gmail.com>
2025-07-26 17:17:18 +0530

    update 23432

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Select all dropdowns
+    const dropdowns = document.querySelectorAll(".navbar .dropdown");
+
+    dropdowns.forEach(function (dropdown) {
+      dropdown.addEventListener("mouseenter", function () {
+        const menu = this.querySelector(".dropdown-menu");
+        const toggle = this.querySelector(".dropdown-toggle");
+        if (menu && toggle) {
+          menu.classList.add("show");
+          toggle.setAttribute("aria-expanded", "true");
+        }
+      });
+
+      dropdown.addEventListener("mouseleave", function () {
+        const menu = this.querySelector(".dropdown-menu");
+        const toggle = this.querySelector(".dropdown-toggle");
+        if (menu && toggle) {
+          menu.classList.remove("show");
+          toggle.setAttribute("aria-expanded", "false");
+        }
+      });
+    });
+  });
+
